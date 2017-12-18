54a2167f19d934414781a21ea7c3b47eb6ee00d7
Haoxin Li <54lihaoxin@gmail.com>
2017-12-18 06:41:11 -0800

    750: Probably not the fastest solution, but fast enough to pass the test... An article talks about treating a row differently by how many 1's in it.

--- Swift/750_number-of-corner-rectangles.playground/Contents.swift
+++ Swift/750_number-of-corner-rectangles.playground/Contents.swift
@@ -37,36 +37,43 @@ class Solution {
             guard row.count == firstRow.count else { return 0 }
         }
         
+        var indexCountDict: [Int: Int] = [:]    // K: pair index, V: count
         let indexOffsetA = 1    // use (i + 1) instead of i because the product is 0 if i is 0
-        let indexOffsetB = indexOffsetA + firstRow.count * firstRow.count  // add offset (i + 1 + rowCount ^ 2) to uniquely identify the 2nd corner
-        let indexSetArray: [Set<Int>] = grid.map { row in
-            var indexSet: Set<Int> = []
+        let indexOffsetB = indexOffsetA + firstRow.count * firstRow.count   // add offset (i + 1 + rowCount ^ 2) to uniquely identify the 2nd corner
+        grid.forEach { row in
             for i in 0..<(row.count - 1) {
                 guard row[i].isCorner else { continue }
                 for ii in (i + 1)..<row.count {
                     guard row[ii].isCorner else { continue }
-                    indexSet.insert((i + indexOffsetA) * (ii + indexOffsetB))   // this identifies a corner pair
-                }
-            }
-            return indexSet
-        }
-        
-        var count = 0
-        for i in 0..<(indexSetArray.count - 1) {
-            let setA = indexSetArray[i]
-            guard !setA.isEmpty else { continue }
-            for ii in (i + 1)..<indexSetArray.count {
-                let setB = indexSetArray[ii]
-                guard !setB.isEmpty else { continue }
-                for index in setB {
-                    if setA.contains(index) {
-                        count += 1
+                    let index = (i + indexOffsetA) * (ii + indexOffsetB)    // this identifies a corner pair
+                    if let count = indexCountDict[index] {
+                        indexCountDict[index] = count + 1
+                    } else {
+                        indexCountDict[index] = 1
                     }
                 }
             }
         }
         
-        return count
+        let pairCounter = PairCounter()
+        return indexCountDict.values.map { pairCounter.numberOfPairs(for: $0) }.reduce(0, +)
+    }
+}
+
+class PairCounter {
+    
+    var resultCache: [Int: Int] = [:]
+    
+    func numberOfPairs(for numberOfPoints: Int) -> Int {
+        if let r = resultCache[numberOfPoints] {
+            return r
+        } else {
+            if numberOfPoints > 1 {
+                return numberOfPoints - 1 + numberOfPairs(for: numberOfPoints - 1)
+            } else {
+                return 0
+            }
+        }
     }
 }
 
@@ -84,4 +91,3 @@ guard 9 == solution.countCornerRectangles([[1, 1, 1],
                                            [1, 1, 1]]) else { fatalError() }
 
 print("All tests passed")
-
