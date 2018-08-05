2bfe1874340fb511e392c0b8e65524fc300398ba
Haoxin Li <54lihaoxin@gmail.com>
2018-08-04 22:31:00 -0700

    Create the `LeetCodeSolutions` project

---
+++ Swift/LeetCodeSolutions/LeetCodeSolutions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCodeSolutions
+//
+//  Created by Li, Haoxin on 8/4/18.
+//  Copyright © 2018 Haoxin Li. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
